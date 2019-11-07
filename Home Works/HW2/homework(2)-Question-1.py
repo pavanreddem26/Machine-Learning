3ae990979624f9d8351ece7c7d62ce4eda2187f0
pkr26 <54822726+pkr26@users.noreply.github.com>
2019-11-07 01:40:09 -0500

    Update homework(2)-Question-1.py

--- Home Works/HW2/homework(2)-Question-1.py
+++ Home Works/HW2/homework(2)-Question-1.py
@@ -1,20 +1,7 @@
 
-# coding: utf-8
-
-# <h2> Question-1A
-# 
-
-# <h3>Linear Regression and Ridge Regression Using Normal Equations and Gradient Descent Iteration Method
-
-# In[1]:
-
-
 import pandas as pd
 import numpy as np
 import os
-
-
-# In[2]:
 
 
 #checking the path directory
@@ -25,38 +12,24 @@ os.chdir(path)
 print("Current directory",os.getcwd())
 
 
-# In[3]:
-
 
 col=["CRIM","ZN","INDUS","CHAS","NOX","RM","AGE","DIS","RAD","TAX","PTRATIO","B","LSTAT","MEDV"]
 train_housing=pd.read_table("housing_train.txt",sep='\s+',names=col)
 train_housing
 
 
-# In[4]:
-
-
 y_train=train_housing['MEDV']
 y_train
-
-
-# In[5]:
 
 
 del train_housing['MEDV']
 train_housing
 
 
-# In[6]:
-
-
 #Now training using Regression Normal Equations an Ridge Regression Normal Equations
 
 w_normal=np.linalg.inv((train_housing.T).dot(train_housing)).dot(train_housing.T).dot(y_train)
 w_normal  #Find the coefficients using the Linear Regression Normal Equations
-
-
-# In[7]:
 
 
 #Now training the same data using the Ridge Regression Normal Equation
@@ -83,13 +56,7 @@ for i in range(n):
     
 
 
-# In[8]:
-
-
 w_ridge
-
-
-# In[9]:
 
 
 #Till now I trained the model but now I need to test and find the error
@@ -102,16 +69,10 @@ def mean_squared_error(train,y,w,length):
     return(squared_error/length)
 
 
-# In[10]:
-
-
 #Training Error
 #This is the mean sqaured error using the Linear regression Normal Equations
 mse_train_linear=mean_squared_error(train_housing,y_train,w_normal,len(train_housing))
 mse_train_linear
-
-
-# In[11]:
 
 
 #Training Error
@@ -121,47 +82,26 @@ for i in range(n):
     mse_train_ridge[i]=mean_squared_error(train_housing,y_train,w_ridge[i],len(train_housing))
 mse_train_ridge
 
-
-# In[12]:
-
-
 #Testing Error
 test_housing=pd.read_csv("housing_test.txt",sep="\s+",header=None,names=col)
 test_housing
 
 
-# In[13]:
-
-
 y_test=test_housing["MEDV"]
 y_test
 
-
-# In[14]:
-
-
 del test_housing["MEDV"]
 test_housing
-
-
-# In[15]:
-
 
 #Since I already trained the model now I can test directly
 mse_test_linear=mean_squared_error(test_housing,y_test,w_normal,len(test_housing))
 mse_test_linear
 
 
-# In[16]:
-
-
 mse_test_ridge=[0 for i in range(n)]
 for i in range(n):
     mse_test_ridge[i]=mean_squared_error(test_housing,y_test,w_ridge[i],len(test_housing))
 mse_test_ridge
-
-
-# In[17]:
 
 
 print("Training Errors:")
@@ -175,9 +115,6 @@ print("Using Ridge Regression(with different ALPHA values):",mse_test_ridge)
 
 # <h3> Linear Regression and Logistic Regression using Gradient descent Iteration method
 
-# In[18]:
-
-
 #Linear Regression uisng Gradient Descent Update rule
 #First Intialize the weights to some random values
 w_reg=[0 for i in range(len(train_housing.columns))]
@@ -186,21 +123,11 @@ for i in range(len(train_housing.columns)):
 #Intialized the values to some random values
 
 
-# In[19]:
-
-
 w=np.reshape(w_reg,((len(train_housing.columns)),1))
 w
 
-
-# In[20]:
-
-
 LR=0.00001
-
-
-# In[21]:
-
+#In this case I am fixing the learning rate but in general we can change the learning rate accordingly...
 
 #Gradient Descent Iteration method for Linear Regression
 #This is mainly for finding the weights(w) using iteration method as opposed to Normal Equations
@@ -221,15 +148,7 @@ for i in range(n):
     print("Round:",i)
     print("Error:",error/len(train_housing))
 
-
-# In[ ]:
-
-
 print(w)
-
-
-# In[ ]:
-
 
 ##Ridge Regression using Gradient Descent Iteration Method
 #Alpha is generally a langrange Constraint and the optimization we are finding is the Constraint Optimization
@@ -244,17 +163,9 @@ for i in range(len(train_housing.columns)):
     w_rid[i]=np.random.uniform(-10,10)
     
 
-
-
-# In[ ]:
-
-
 #Changing the Shape of the w's so that it can support Matrix Multiplication
 w_rid_gd=np.reshape(w_rid,((len(train_housing.columns)),1))
 w_rid_gd
-
-
-# In[ ]:
 
 
 #Gradient Descent Rule for Ridge Regression
@@ -280,8 +191,7 @@ for i in range(n_1):
     print("Error:",error/(len(train_housing)))
 
 
-# In[ ]:
-
-
 w_rid_gd
+
+#Till here I trained the model using the Linear and Ridge Regression Normal Equations and also using the Gradient Descent Update Rule
 
